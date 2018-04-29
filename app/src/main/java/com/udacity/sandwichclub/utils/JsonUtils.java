3996d94a01bdbc2401794afa825252ce220c83af
nhutch01 <nhutch01@gmail.com>
2018-04-28 22:10:46 -0500

    First commit of Sandwich Club Application

---
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -0,0 +1,43 @@
+package com.udacity.sandwichclub.utils;
+
+import com.udacity.sandwichclub.model.Sandwich;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class JsonUtils {
+
+    public static Sandwich parseSandwichJson(String json) {
+        try{
+            JSONObject jsonObject = new JSONObject(json);
+            JSONObject name = jsonObject.optJSONObject("name");
+            String mainName = name.optString("mainName");
+            JSONArray alsoKnownAsJSONAry = name.optJSONArray("alsoKnownAs");
+            ArrayList<String> alsoKnownAsStringAry = new ArrayList<>();
+
+            for(int i=0; i<alsoKnownAsJSONAry.length();i++){
+                alsoKnownAsStringAry.add(alsoKnownAsJSONAry.getString(i));
+            }
+
+            String placeOfOrigin = name.optString("placeOfOrigin");
+            String description = name.optString("description");
+            String image = name.optString("image");
+
+            JSONArray ingredientsJSONAry = name.optJSONArray("ingredients");
+            ArrayList<String> ingredientsStringAry = new ArrayList<>();
+
+            for(int i=0; i<ingredientsJSONAry.length();i++){
+                ingredientsStringAry.add(ingredientsJSONAry.getString(i));
+            }
+
+            return new Sandwich(mainName,alsoKnownAsStringAry,placeOfOrigin,description,image,ingredientsStringAry);
+        }catch(JSONException e){
+            e.printStackTrace();
+            return null;
+        }
+
+    }
+}
